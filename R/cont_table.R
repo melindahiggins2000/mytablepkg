5fa63689bb8ce726de02fc8aa1b84c6da8f71daf
Melinda Higgins <melinda.higgins@emory.edu>
2025-09-13 17:44:49 -0400

    add files for mytablepkg

---
+++ R/cont_table.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @param varlist list of variables for table
+#' @param data data.frame for dataset
+#'
+#' @returns a gtsummary table of class "tbl_summary"
+#' @export
+#'
+#' @examples
+#' cont_table(varlist = c("mpg", "wt"),
+#'            data = mtcars)
+cont_table <- function(varlist, data){
+  gtsummary::tbl_summary(
+    data = data,
+    include = tidyselect::any_of(varlist),
+    statistic = list(gtsummary::all_continuous() ~ "{mean} ({sd})"),
+    digits = gtsummary::all_continuous() ~ 2
+  )
+}
